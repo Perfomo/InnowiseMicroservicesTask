0eb946b7a6d4e021c38efb0104113a79cda27685
Kirill Toleyko <68685003+Perfomo@users.noreply.github.com>
2024-05-15 12:01:22 +0300

    Delete inventory-service directory

--- inventory-service/src/main/java/com/toleyko/springboot/inventoryservice/handlers/exception/RemainderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.toleyko.springboot.inventoryservice.handlers.exception;
-
-public class RemainderNotFoundException extends Exception {
-    public RemainderNotFoundException(String info) {
-        super(info);
-    }
-}
